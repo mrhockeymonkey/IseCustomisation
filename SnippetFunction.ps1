6f909edf74a5f4e1033b44fef522903015cae1c1
mrhockeymonkey <s.a.matthews@live.co.uk>
2016-09-17 15:25:45 +0100

    First commit

---
+++ SnippetFunction.ps1
@@ -0,0 +1,41 @@
+﻿$Snippet = @{
+    Title = 'A Function (+help)'
+    Author = 'Scott Matthews'
+    Description = 'Framework for a function'
+    Force = $true
+    Text = @' 
+<#
+    .SYNOPSIS
+        Short description
+    
+    .DESCRIPTION
+        Long description
+    
+    .PARAMETER Name
+        Description of parameter
+    
+    .EXAMPLE
+        Example of how to use this cmdlet
+    
+    .EXAMPLE
+        Another example of how to use this cmdlet
+#>
+   
+Function Verb-Noun {
+    [CmdletBinding()]
+    Param (
+
+    )
+
+    Begin {}
+
+    Process {}
+
+    End {}
+}
+'@
+}
+
+Write-Verbose "Adding Snippet: Function" -Verbose
+Get-IseSnippet | ?{$_.Name -eq "$($Snippet.Title).snippets.ps1xml"} | Remove-Item
+New-IseSnippet @snippet
